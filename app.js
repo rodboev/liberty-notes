8a0f434abd07433f4fe2d5583ffa6edf6e85b3bd
Rod Boev <rod.boev@gmail.com>
2022-08-18 12:44:44 -0400

    TODO all keywords replaced in single pass

--- app.js
+++ app.js
@@ -39,22 +39,30 @@ const categories = [
 ]
 
 const keywords = categories.flatMap(c => c.keywords)
-console.log(`---\nLooking for keywords: ${keywords.join(', ')}`);
+// console.log(`---\nLooking for keywords: ${keywords.join(', ')}`);
 
-let keywordsFound = [];
-for (note of notes) {
-  console.log(`\nTesting note: ${note}`);
-  keywordsFound = [];
-  for (keyword of keywords) {
-    const regex = new RegExp(keyword, 'g');
-    if (note.match(keyword)) {
+const matchingNotes = [];
+const keywordsFound = [];
+
+for (const note of notes) {
+  // Get a full view of all matching keywords, if any
+  for (const keyword of keywords) {
+    const regex = new RegExp(keyword, 'gi');
+    if (note.match(regex)) {
       keywordsFound.push(keyword);
+      // Replace matching keyword with <<highlight>>. To be categorized, ordered and limited to single, highest priority keyword
+      matchingNotes.push(note.replaceAll(keyword, `<<${keyword}>>`));
     }
   }
+
+  /*
   if (keywordsFound.length > 0) {
-    console.log(`Keywords found: ${keywordsFound}`);
+    // replace each note keyword with <<keyword>>
+    console.log(note.replaceAll(keyword, `<<${keyword}>>`));
   }
-  else {
-    console.log(`No keywords found, skipping this note.`);
-  }
+  */
 }
+
+console.log(matchingNotes.join(`\n\n`));
+console.log(`Found ${matchingNotes.length} of ${notes.length} notes with matching keywords.`);
+// console.log(`Keywords found (${keywordsFound.length}): ${keywordsFound.join(', ')}`);
