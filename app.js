5bd3069c22e5ff06799941ea600ac3b88c709f59
Rod Boev <rod.boev@gmail.com>
2022-08-30 01:32:25 -0400

    Work around encoding issues

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const fs = require('fs')
+const path = require('path')
 
 const express = require('express')
+const encoding = require('encoding')
 
 const app = express()
 const port = parseInt(process.env.PORT) || 3000
+
+app.get('/notes.json', (req, res) => {
+  fs.readFile(path.join('public', 'notes.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.send(encoding.convert(data, 'windows-1252', 'utf-8'))
+  })
+})
 
 app.use(express.static('public'))
 
