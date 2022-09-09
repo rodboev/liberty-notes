1c0b04b0be35ae189c9d7449fcc18da3c7eb047b
Rod Boev <rod.boev@gmail.com>
2022-09-09 12:25:12 -0400

    Go back to CommonJS, start csv2json integration

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs');
+const path = require('path');
 
-import express from 'express'
-import iconv from 'iconv-lite';
-const {encode, decode} = iconv;
+const converter = require('json-2-csv');
+
+const express = require('express');
+var iconv = require('iconv-lite');
 
 const app = express()
 const port = parseInt(process.env.PORT) || 3000
 
-app.get('/notes.json', (req, res) => {
-  fs.readFile(path.join('__dirname', 'notes.json'), 'utf8', async(err, data) => {
-    if (err) throw err
-    const buf = iconv.encode(data, 'win1252')
-    const decoded = iconv.decode(buf, 'utf8')
-    res.send(decoded)
-  })
+app.get('/api/:id', (req, res) => {
+  const fileName = req.params.id
+  const readFile = async (fileName) => {
+    const urlParts = fileName.split('.')
+    const fileExt = urlParts[1]
+
+    if (fileName === 'upload') {
+    }
+    else {
+      fs.readFile(fileName, 'utf8', async(err, data) => {
+        if (err) throw err
+
+        if (fileExt === 'csv') {
+          data = await converter.csv2jsonAsync(data, {keys: ['Note']})
+        }
+
+        // Get rid of weird symbols in output
+        const buf = iconv.encode(data, 'win1252')
+        const decoded = iconv.decode(buf, 'utf8')
+
+        res.send(decoded)
+      })
+    }
+  }
+  readFile(fileName)
 })
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  const timestamp = new Date().toLocaleTimeString();
+	console.log(`> [${timestamp}] App listening on port ${port}...`);
 })
