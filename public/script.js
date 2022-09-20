bec4b2bddcd258a320fdb3b9f409bbb56983b492
Rod Boev <rod.boev@gmail.com>
2022-09-20 06:57:04 -0400

    Apparently working priorities and duplicate remove

--- public/script.js
+++ public/script.js
@@ -26,6 +26,9 @@ const categories = [
     keywords: [
       'key',
       'keys',
+      'gate',
+      'lock',
+      'pin',
       'lockbox',
       'lock box',
     ],
@@ -45,101 +48,103 @@ const categories = [
     ],
     notes: []
   },
-	{
-		name: 'UNCATEGORIZED',
-		keywords: [],
-		notes: []
-	}
+  {
+    name: 'UNCATEGORIZED',
+    keywords: [],
+    notes: []
+  }
 ]
 
 const formatNotes = (notes) => {
-	const formattedNotes = [];
-	for (let note of notes) {
-		if (note.hasOwnProperty('Note')) {
-			note = `<h5>Name:</h5><a href="https://app.pestpac.com/location/detail.asp?LocationID=${note['Location ID']}">${note['Company']}</a><br />
-				<h5>Code:</h5><a href="https://app.pestpac.com/location/detail.asp?LocationID=${note['Location ID']}">${note['Location Code']}</a><br />
-				<h5>Note:</h5><span class="note">${note['Note']
-					.replace(/^Service: /, "")
-					.replace(/\s{2,}/g, "<br />")
-				}</span><br />
-				<h5>From:</h5>${note['Added By']}</span>`
-				formattedNotes.push(note)
-		}
-		else {
-			alert("Couldn't find a \"Note\" column")
-			break
-		}
-	}
-	return formattedNotes
+  const formattedNotes = [];
+  for (let note of notes) {
+    if (note.hasOwnProperty('Note')) {
+      note = `<h5>Name:</h5><a href="https://app.pestpac.com/location/detail.asp?LocationID=${note['Location ID']}">${note['Company']}</a><br />
+        <h5>Code:</h5><a href="https://app.pestpac.com/location/detail.asp?LocationID=${note['Location ID']}">${note['Location Code']}</a><br />
+        <h5>Note:</h5><span class="note">${note['Note']
+          .replace(/^Service: /, "")
+          .replace(/\s{2,}/g, "<br />")
+        }</span><br />
+        <h5>From:</h5>${note['Added By']}</span>`
+        formattedNotes.push(note)
+    }
+    else {
+      alert("Couldn't find a \"Note\" column")
+      break
+    }
+  }
+  return formattedNotes
 }
 
 const groupNotes = (notes) => {
-	const keywords = categories.flatMap(c => c.keywords)
+  const keywords = categories.flatMap(c => c.keywords)
 
-	for (const note of notes) {
-		// Highlight keywords in notes
-		const pattern = new RegExp(keywords.join('\\b|\\b'), 'gi'); 
-		const highlightedNote = note.replace(pattern, match => `<span class='highlight'>${match}</span>`)
-		
-		// If no highlights, push to last category
-		if (!note.indexOf(highlightedNote)) {
-			categories[categories.length-1].notes.push(note);
-		}
+  for (const note of notes) {
+    // Highlight keywords in notes
+    const pattern = new RegExp(keywords.join('\\b|\\b'), 'gi'); 
+    const highlightedNote = note.replace(pattern, match => `<span class='highlight'>${match}</span>`)
+    
+    // If no highlights, push to last category
+    if (!note.indexOf(highlightedNote)) {
+      categories[categories.length-1].notes.push(note);
+    }
 
-		// Organize into groups
-		for (const category of categories) {
-			for (const keyword of category.keywords) {
-				const pattern = new RegExp('\\b' + keyword + '\\b', 'gi'); 
-				if (pattern.test(highlightedNote)) {
-					category.notes.push(highlightedNote)
-					break // TODO: Check for keyword priority
-				}
-			}
-		}
-	}
+    // Organize into groups
+    let prioritizedNotes = []
+    for (const category of categories) {
+      for (const keyword of category.keywords) {
+        const pattern = new RegExp('\\b' + keyword + '\\b', 'gi'); 
+        if (pattern.test(highlightedNote)) {
+          for (const notes of notes.entries) {
+            prioritizedNotes.push(category.notes[categories.note.priority - 1])
+          }
+        }
+      }
+    }
+  }
 }
 
 const fetchNotes = async (json = '/api/notes.json') => {
-	const response = await fetch(json)
-	const notes = await response.json()
-	return notes;
+  const response = await fetch(json)
+  const notes = await response.json()
+  return notes;
 }
 
 const displayNotes = (notes) => {
-	let data = '';
-	for (const category of categories) {
-		data += `<h4>${category.name}</h4>`
-		for (let [i, note] of category.notes.entries()) {
-			data += `<ul><span class="num">${i + 1}.</span>${note}</ul>`
-		}
-	}
-	return data;
+  let data = '';
+  for (const category of categories) {
+    data += `<h4>${category.name}</h4>`
+    for (let [i, note] of category.notes.entries()) {
+      data += `<ul><span class="num">${i + 1}.</span>${note}</ul>`
+    }
+  }
+  return data;
 }
 
 const refreshNotes = async () => {
-	for (const category of categories) {
-		category.notes.length = 0
-	}
-	const notes = await fetchNotes()
-	const formattedNotes = formatNotes(notes)
-	const groupedNotes = groupNotes(formattedNotes)
-	const content = document.querySelector('.content')
-	content.innerHTML = displayNotes(groupedNotes)
+  for (const category of categories) {
+    category.notes.length = 0
+  }
+  const notes = await fetchNotes()
+  const groupedNotes = groupNotes(notes)
+  const formattedNotes = formatNotes(groupedNotes)
+  const content = document.querySelector('.content')
+  content.innerHTML = displayNotes(groupedNotes)
 }
 
 async function saveFile(input) {
-	let formData = new FormData()
-	const file = input.files[0]
-	const ext = file.name.indexOf('.') != -1 &&
-		file.name.substr(file.name.lastIndexOf('.') + 1, file.name.length)
-	if (ext === 'csv') {
-		formData.append("file", file)
-		await fetch('/api/upload', {method: "POST", body: formData})
-		refreshNotes()
-	}
-	else {
-		alert("Please choose a file with a file with a .csv extension.")
-	}
+  let formData = new FormData()
+  const file = input.files[0]
+  const ext = file.name.indexOf('.') != -1 &&
+    file.name.substr(file.name.lastIndexOf('.') + 1, file.name.length)
+  if (ext === 'csv') {
+    formData.append("file", file)
+    await fetch('/api/upload', {method: "POST", body: formData})
+    refreshNotes()
+  }
+  else {
+    alert("Please choose a file with a file with a .csv extension.")
+  }
 }
 
 const input = document.querySelector('input[type="file"]')
